ebdbc8e4d767c9252013a4e912313185bc202cff
Amir <dahmouni_amir@hotmail.fr>
2023-05-14 15:32:07 +0100

    reafacto

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,9 +12,11 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
 
   const skeletons = [1, 2, 3, 4, 5, 6];
+
+  if (error) return <Text>{error}</Text>
+
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6} margin='10px' >
         {isLoading && skeletons.map(skeleton => (
           <GameCardContainer key={skeleton}>
